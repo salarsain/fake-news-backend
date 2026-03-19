4f8c57b3d137df48856a42da33322924901e6fb3
salarsain <salarsain2456@gmail.com>
2026-03-19 06:59:46 +0500

    fix cors allow all

--- main.py
+++ main.py
@@ -19,14 +19,11 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://fake-news-detector-393.pages.dev",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
